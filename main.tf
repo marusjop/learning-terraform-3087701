e4d341df8972541b4d34a7dda2cb693330dce354
marusjop <101182403+marusjop@users.noreply.github.com>
2023-07-20 10:21:23 +0200

    Update main.tf with ubuntu image

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+#  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
