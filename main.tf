e432e1fe32c74df31aba3fe4c74a5e4f5ee34731
marusjop <101182403+marusjop@users.noreply.github.com>
2023-07-20 16:32:36 +0200

    Ccos

--- main.tf
+++ main.tf
@@ -20,15 +20,14 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  #azs             = ["eu-north-1", "eu-north-1b", "eu-north-1c"]
-  azs             = ["eu-north-1"]
-  #public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   private_subnets = ["10.0.102.0/24"]
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
