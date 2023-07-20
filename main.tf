dff8a50a2af52a00bff64c36736aec11847a35d9
marusjop <101182403+marusjop@users.noreply.github.com>
2023-07-20 15:52:41 +0200

    axs change

--- main.tf
+++ main.tf
@@ -20,7 +20,8 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-north-1", "eu-north-1b", "eu-north-1c"]
+  #azs             = ["eu-north-1", "eu-north-1b", "eu-north-1c"]
+  azs             = ["eu-north-1"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
