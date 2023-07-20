77f0e40653275177f64edd068828563dbd0b72bd
marusjop <101182403+marusjop@users.noreply.github.com>
2023-07-20 16:44:25 +0200

    dsad

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ module "vpc" {
 
   azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  private_subnets = ["10.0.104.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  
   enable_nat_gateway = true
 
   tags = {
